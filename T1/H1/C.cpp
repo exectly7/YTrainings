a10f781153142190187de2c6d3b6b809081429b6
exxxy <iburov7@icloud.com>
2025-04-06 14:07:45 +0300

    T1 H1 C-

---
+++ T1/H1/C.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string newNumber = "";
+  std::string exNumber1 = "";
+  std::string exNumber2 = "";
+  std::string exNumber3 = "";
+  std::cin >> newNumber >> exNumber1 >> exNumber2 >> exNumber3;
+  
+  return 0;
+}
